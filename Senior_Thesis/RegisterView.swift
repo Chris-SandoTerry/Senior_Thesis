8ad88f5a8ab491c5058ad6b56cc5047ce01c00dd
Christopher Sandoval Terry <Chrisandoterry64@gmail.com>
2023-11-27 13:32:15 -0600

    Progress is back

--- Senior_Thesis/RegisterView.swift
+++ Senior_Thesis/RegisterView.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RegisterView: View {
+    @State private var showStudentScene = false
+    @State private var showProfessorScene = false
+    let uni = unilist 
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Choose to which apply")
+                    .font(.title)
+                    .padding()
+
+                Button(action: {
+                    // Handle Student button tap
+                    showStudentScene.toggle()
+                }) {
+                    NavigationLink(destination: StudentScene(uni: uni), isActive: $showStudentScene) {
+                        Text("Student")
+                            .foregroundColor(.blue)
+                            .padding()
+                    }
+                }
+
+                Button(action: {
+                    // Handle Professor button tap
+                    showProfessorScene.toggle()
+                }) {
+                    NavigationLink(destination: ProfessorScene(), isActive: $showProfessorScene) {
+                        Text("Professor")
+                            .foregroundColor(.green)
+                            .padding()
+                    }
+                }
+
+                Spacer()
+            }
+            .navigationTitle("Apply")
+        }
+    }
+}
+
+struct StudentScene: View {
+    let uni: [String]
+
+    var body: some View {
+        List(uni, id: \.self) { university in
+            NavigationLink(destination: Text(university)) {
+                Image(systemName: "cap")
+                Text(university)
+            }
+            .padding()
+        }
+        .navigationTitle("Universities")
+    }
+}
+
+struct ProfessorScene: View {
+    var body: some View {
+        Text("Professor Scene")
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
